c6956cc67df02494a227626f0cd843970d7dfc20
k.sato <keisuke.sato.gp@gmail.com>
2018-03-17 23:32:08 +0900

    add gulp-clean-css,uglify

--- gulpfile.js
+++ gulpfile.js
@@ -8,15 +8,19 @@ var sass = require('gulp-sass');
 var plumber = require('gulp-plumber');
 var sourcemaps = require('gulp-sourcemaps');
 var browserSync = require('browser-sync');
+//var uglify = require('gulp-uglify');
+var cleanCSS = require('gulp-clean-css');
 
 // sass自動コンパイルタスク
 gulp.task('sass', function(){
 	gulp.src('./src/scss/**.scss')
 	.pipe(plumber())
 	.pipe(sourcemaps.init())
-	.pipe(sass({
-		outputStyle: 'expanded'
-	}))
+	.pipe(sass(
+		// {
+		// outputStyle: 'expanded'
+		// }
+	))
 	// Sassのコンパイルエラーを表示
 	// (これがないと自動的に止まってしまう)
 	//.on('error', sass.logError)
@@ -28,14 +32,28 @@ gulp.task('sass', function(){
 gulp.task('browser-sync', function() {
     browserSync({
         server: {
-			 //baseDir: "html"
-			 baseDir: ["html", "src"] //対象ディレクトリ
-            //,index  : "index.html" //インデックスファイル
-        }
-    });
+			//baseDir: "html"
+			baseDir: ["html", "src"] //対象ディレクトリ
+			//,index  : "gulp.html" //インデックスファイル
+		}
+	});
 });
 gulp.task('bs-reload', function () {
-    browserSync.reload();
+	browserSync.reload();
+});
+
+//JS圧縮
+// gulp.task('minify-js', function() {
+// 	return gulp.src("./html/common/js/*.js")
+// 	.pipe(uglify())
+// 	.pipe(gulp.dest('./html/common/js')); // 上書き
+// });
+
+//CSS圧縮
+gulp.task('minify-css', function() {
+	return gulp.src("./html/common/css/*.css")
+	.pipe(cleanCSS())
+	.pipe(gulp.dest('./html/common/css')); // 上書き
 });
 
 // watchタスク(html,js,sassファイル変更時に実行するタスク)
@@ -43,6 +61,9 @@ gulp.task('sass-watch', ['sass','browser-sync'], function(){
 	gulp.watch("./src/**/*.scss", ['sass']);
 	gulp.watch("./html/*.html",['bs-reload']);
 	gulp.watch("./src/**/*.scss",['bs-reload']);
+	gulp.watch("./html/common/js/*.js",['bs-reload']);
 });
 
 gulp.task('default', ['sass-watch','browser-sync']);
+//gulp.task('build',['minify-css','minify-js']);
+gulp.task('build',['minify-css']);
